e2575245d790cc4874bd1d8408a114747313fae5
Quentin Danneville <quentin@nextmap.io>
2019-04-13 19:34:36 +0200

    [Back] I’m now using mysql instead of mongodb shit ahaha (:)

--- app/server/_helpers/db.js
+++ app/server/_helpers/db.js
@@ -1,8 +1,39 @@
-const config = require('config.json');
-const mongoose = require('mongoose');
-mongoose.connect(process.env.MONGODB_URI || config.connectionString, { useCreateIndex: true, useNewUrlParser: true });
-mongoose.Promise = global.Promise;
+const config = require("config.json");
+const mysql = require("mysql");
 
-module.exports = {
-    User: require('../users/user.model')
-};
+// const mongoose = require('mongoose');
+// mongoose.connect(process.env.MONGODB_URI || config.connectionString, { useCreateIndex: true, useNewUrlParser: true });
+// mongoose.Promise = global.Promise;
+
+const db = mysql.createConnection({
+  host: "localhost",
+  user: "qdan",
+  password: "derpderp",
+  database: "app"
+});
+
+// connect to database
+db.connect(err => {
+  if (err) {
+    throw err;
+  }
+  console.log("Connected to database");
+});
+
+global.db = db;
+
+module.exports = db;
+
+let query = "SELECT * FROM `user`";
+
+// db.query(query, (err, result) => {
+//   if (err) {
+//     console.error(err);
+//   } else {
+//     console.log(result);
+//   }
+// });
+
+// module.exports = {
+//     User: require('../users/user.model')
+// };
